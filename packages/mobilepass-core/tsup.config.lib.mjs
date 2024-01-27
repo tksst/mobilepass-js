1133a84bc2586e0451403ed75da76e8e1ff72567
Takashi Sato <takashi@tks.st>
2024-01-27 19:15:47 +0900

    set Node.js and Safari versions

--- packages/mobilepass-core/tsup.config.lib.mjs
+++ packages/mobilepass-core/tsup.config.lib.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
     ...libOptions,
     entry: ["src/lib/index.ts"],
+    target: ["node16.14.0", "safari15"],
 
     // If you know that this library is for Node.js or for a browser, you may want to set the platform.
     // platform: "node",

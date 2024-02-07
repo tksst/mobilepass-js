b30bd7440b3bcbbd672bbef24f4e56912a68cea6
Takashi Sato <takashi@tks.st>
2024-02-08 00:24:27 +0900

    lib main code

--- packages/mobilepass-core/src/lib/main/main.ts
+++ packages/mobilepass-core/src/lib/main/main.ts
@@ -1,3 +1,9 @@
-export const main = (): void => {
-    // console.log("This is a template for Node.js CLI or Library project in TypeScript.");
-};
+import { hotpSHA256 } from "../utils/hotp.js";
+import { activationCodeToKey, fixedKdfSha256 } from "../utils/utils.js";
+
+export async function generate(activationCode: string, counter: number | bigint): Promise<string> {
+    const key1 = activationCodeToKey(activationCode);
+    const key2 = await fixedKdfSha256(key1);
+
+    return await hotpSHA256(key2, counter);
+}
--- packages/mobilepass-core/tsup.config.node.mjs
+++ packages/mobilepass-core/tsup.config.node.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
     ...libOptions,
-    entry: ["src/lib/index.ts"],
-    target: ["safari15"],
+    entry: ["src/lib/node.ts"],
+    target: ["node15.0.0"],
 
     // If you know that this library is for Node.js or for a browser, you may want to set the platform.
     // platform: "node",

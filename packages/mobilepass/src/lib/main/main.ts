dfdb322113cde3ae39a3ff5dd314b90bdb141c81
Takashi Sato <takashi@tks.st>
2024-02-11 14:04:47 +0900

    lib main code

--- packages/mobilepass/src/lib/main/main.ts
+++ packages/mobilepass/src/lib/main/main.ts
@@ -1,3 +1,15 @@
-export const main = (): void => {
-    // console.log("This is a template for Node.js CLI or Library project in TypeScript.");
-};
+import { hotpSHA256 } from "../utils/hotp.js";
+import { activationCodeToKey, fixedKdfSha256 } from "../utils/utils.js";
+
+/**
+ * Generate an One-Time Password (OTP)
+ * @param activationCode Your activation code
+ * @param counter counter
+ * @returns Generated an One-Time Password (OTP)
+ */
+export async function generateOtp(activationCode: string, counter: number | bigint): Promise<string> {
+    const key1 = activationCodeToKey(activationCode);
+    const key2 = await fixedKdfSha256(key1);
+
+    return await hotpSHA256(key2, counter);
+}

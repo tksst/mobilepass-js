05ddc49bf2129e941520dba488f3de7c2181e21c
Takashi Sato <takashi@tks.st>
2024-02-12 12:21:09 +0900

    remove executable stuff

--- packages/mobilepass/tsup.config.bin.mjs
+++
@@ -1,7 +0,0 @@
-import { binOptions } from "@tksst/project-configs/tsup-config.mjs";
-import { defineConfig } from "tsup";
-
-export default defineConfig({
-    ...binOptions,
-    entry: ["src/bin/index.ts"],
-});
